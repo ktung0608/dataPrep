32910bfe2d5c1d3d6a32a9fab5aa9509e992cdfb
jidokasg <kenny.tgw@gmail.com>
2021-07-18 17:33:49 +0800

    updated 18 jul

--- datadiy.py
+++ datadiy.py
@@ -1,13 +1,55 @@
+from matplotlib.pyplot import xcorr
 import streamlit as st
 import pandas as pd
 import numpy as np
 import time
 import os 
 import base64
+from pandas.api.types import is_numeric_dtype
+import missingno as msno
+
 
 st.set_page_config(layout="wide")
 
-st.title('DataDIY')
+st.sidebar.write("1. Upload CSV file (currently CSV only)")
+st.sidebar.write("2. Dataframe shows your data real time")
+st.sidebar.write("3. Missingno show completeness of data")
+st.sidebar.write("4. Data fields to keep")
+st.sidebar.write(" -- Keep blank to retain all colums")
+st.sidebar.write(" -- Select columns to resize dataframe")
+st.sidebar.write("5. Replace null values")
+st.sidebar.write(" -- Replace with 0, mean, median")
+st.sidebar.write(" -- It will only work for numeric fields")
+st.sidebar.write("6. Remove null values")
+st.sidebar.write(" -- Keep blank to remove all rows with null")
+st.sidebar.write(" -- Select columns to remove null if exist in selected column")
+st.sidebar.write("7. Replace values")
+st.sidebar.write(" -- Keep blank to replace across all data fields")
+st.sidebar.write(" -- Select column to effect change on selected column")
+st.sidebar.write("8. Show distribution range")
+st.sidebar.write(" -- Only works for numeric values.")
+st.sidebar.write(" -- Show distribution of values for exploration")
+st.sidebar.write("9. Show unique values")
+st.sidebar.write(" -- Show unique values in selected column")
+st.sidebar.write("10. Download File")
+st.sidebar.write(" -- Export modified file in csv format")
+
+
+@st.cache
+
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+# Create a text element and let the reader know the data is loading.
+#data_load_state = st.text('Loading data...')
+
+# Notify the reader that the data was successfully loaded.
+#data_load_state.text("Cache Loaded! (using st.cache)")
+
 
 #====== DOWNLOAD FILE FUNCTION ======#
 
@@ -33,172 +75,181 @@ def download_link(object_to_download, download_filename, download_link_text):
 
 #======================================
 
+st.title('DataDIY')
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
-
-@st.cache
-
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
-
-# Create a text element and let the reader know the data is loading.
-data_load_state = st.text('Loading data...')
-# Load 10,000 rows of data into the dataframe.
-data = load_data(10000)
-# Notify the reader that the data was successfully loaded.
-data_load_state.text("Done! (using st.cache)")
+tcol1, tcol2, tcol3 = st.beta_columns((1,1,1))
 
 
-tcol1, tcol2, tcol3,tcol4 = st.beta_columns((2,2,1,1))
-col1_sub1, col1_sub2, col1_sub3, col1_sub4, col1_sub5, col1_sub6 = st.beta_columns(6)
-bcol1, bcol2, bcol3 = st.beta_columns(3)
+def datacleanser():
+    
+    # create UI container
 
-file1 = tcol1.file_uploader('Upload File 1')
+    uploaded_file = tcol1.file_uploader('Upload CSV File')
 
-if file1:
-    df1 = pd.read_csv(file1)
-
-    col_list_1 = list()
-
-    for c in df1:
-        col_list_1.append(c)
-
-    selected_col_df1 = tcol1.multiselect('Select Columns to use [File 1]', col_list_1)
-
-    if selected_col_df1:
-        df1_1 = df1[selected_col_df1]
-    else:
-        selected_col_df1 = col_list_1
-        df1_1 = df1[selected_col_df1]
-
-    #=================================
+    # to show following if 
+    if uploaded_file:
+        
+        # read raw data file
+        df_raw = pd.read_csv(uploaded_file)
 
 
+        ### end of first segment ###
+#=================================================================================================================================================================
+        
+        #create second segment
+        #this segment is to hold 3 functions
+        
+        ## [a] Choose what column to keep
+        ## [b] Replace NULL value
+        ## [c] Remove NULL value
 
-    col_list_2 = list()
+        # create UI container
+        mcol1, mcol2, mcol3 = st.beta_columns((1,1,1))
 
-    for c in df1_1:
-        col_list_2.append(c)
+        # create empty list to get columns of raw dataframe
+        col_list_raw = list()
+        for col in df_raw:
+            col_list_raw.append(col)
 
-    selected_col_df1_1_null = col1_sub1.multiselect('Select Columns to remove NULL [File 1]', col_list_2)
-    if selected_col_df1_1_null:
+        # create expander for [a]
+        choose_col_expander = mcol1.beta_expander("Data fields to keep")
 
-        pass
-    else:
-        selected_col_df1_1_null = selected_col_df1
+        # create a NEW dataframe based on user selection
+        selected_col = choose_col_expander.multiselect('Select data fields to keep. If none selection, all data fields will be retained',col_list_raw)
+        if selected_col:
+            df_modify = df_raw[selected_col]
+        else:
+            selected_col = col_list_raw
+            df_modify = df_raw[selected_col]
 
+        # create column list based on modified dataframe
+        col_list_modify = list()
 
-    if col1_sub1.checkbox('Remove NaN rows'):
-        df1_1 = df1_1.dropna(subset=selected_col_df1_1_null)
+        for col in df_modify:
+            col_list_modify.append(col)
 
-    #=================================
+#=================================================================================================================================================================
+        # create expander for [b]
+        replace_null_expander = mcol1.beta_expander("Replace NULL values")
 
-    selected_col_df1_1_replace = col1_sub2.multiselect('Select Columns to replace NULL values [File 1]', col_list_2)
+        selected_col_replace = replace_null_expander.multiselect('Select Columns to replace NULL values [File 1]', col_list_modify)
 
-    if selected_col_df1_1_replace:
-        pass
-    else:
-        selected_col_df1_1_replace = selected_col_df1
+        if selected_col_replace:
+            pass
+        else:
+            selected_col_replace = col_list_modify
 
-    replace_null_f1 = col1_sub2.radio('Replace NULL with [File 1]:', ('No action','0','Mean', 'Median'))
+        replace_null = replace_null_expander.radio('Replace NULL:', ('No action','0','Mean', 'Median'))
 
-    if replace_null_f1 == 'No action':
-        pass
-    if replace_null_f1 == '0':
-        df1_1[selected_col_df1_1_replace] = df1_1[selected_col_df1_1_replace].fillna(0)
-    if replace_null_f1 == 'Mean':
-        df1_1[selected_col_df1_1_replace] = df1_1[selected_col_df1_1_replace].fillna(df1_1.mean())
-    if replace_null_f1 == 'Median':
-        df1_1[selected_col_df1_1_replace] = df1_1[selected_col_df1_1_replace].fillna(df1_1.median())
+        if replace_null == 'No action':
+            pass
+        if replace_null == '0':
+            df_modify[selected_col_replace] = df_modify[selected_col_replace].fillna(0)
+        if replace_null == 'Mean':
+            df_modify[selected_col_replace] = df_modify[selected_col_replace].fillna(df_modify[selected_col_replace].mean())
+        if replace_null == 'Median':
+            df_modify[selected_col_replace] = df_modify[selected_col_replace].fillna(df_modify[selected_col_replace].median())
 
+#=================================================================================================================================================================
 
-    bcol1.write(df1_1)
-    bcol1.write(df1_1.shape)
+        # create expander for [c]
+        remove_null_expander = mcol1.beta_expander("Remove NULL values")
 
+        selected_col_remove = remove_null_expander.multiselect('Select data fields to have rows removed if NULL. If no selection, all columns will be used', col_list_modify)
+        if selected_col_remove:
+            pass
+        else:
+            selected_col_remove = col_list_modify
 
-    if bcol1.button('Download [File 1] as CSV'):
-        tmp_download_link_file_1 = download_link(df1_1, 'file_1.csv', 'Click here to download your data!')
-        st.markdown(tmp_download_link_file_1, unsafe_allow_html=True)
+        if remove_null_expander.checkbox('Remove NULL rows'):
+            df_modify = df_modify.dropna(subset=selected_col_remove)
 
+#=================================================================================================================================================================
 
-    #=================================
+        ## [d] Replace value
+        ## [e] Spare Placeholder
+        ## [f] Spare Placeholder
 
+        # create expander for [f]
+        replace_value= mcol2.beta_expander("Replace Values")
 
+        selected_col_replace_value = replace_value.multiselect('Select columns to replace', col_list_modify)
+        
+        old_value= replace_value.text_input('Old Value')
+        new_value= replace_value.text_input('New Value')
 
-###FILE 2####
+        if old_value != "" and new_value != "":
+            if selected_col_replace_value:
+                df_modify[selected_col_replace_value] = df_modify[selected_col_replace_value].replace(old_value, new_value)
+                replace_value.write('Values replaced')
 
-file2 = tcol2.file_uploader('Upload File 2')
+            else:
+                df_modify = df_modify.replace(old_value,new_value)
+                replace_value.write('Values replaced')
 
-if file2:
-    df2 = pd.read_csv(file2)
+        placeholder_1= mcol2.beta_expander("placeholder_1")
+        placeholder_2= mcol2.beta_expander("placeholder_2")
 
-    col_list_3 = list()
+#=================================================================================================================================================================
 
+        ## [g] Replace value
+        ## [h] Spare Placeholder
+        ## [i] Spare Placeholder
 
-    for c in df2:
-        col_list_3.append(c)
+        # create expander for [g]
+        distribution_expander = mcol3.beta_expander("Show distribution range (for numeric values only)")
 
-    selected_col_df2 = tcol2.multiselect('Select Columns to use [File 2]', col_list_3)
+        selected_col_distribution = distribution_expander.selectbox('Select data field to display distribution', col_list_modify)
 
-    if selected_col_df2:
-        df2_1 = df2[selected_col_df2]
-    else:
-        selected_col_df2 = col_list_3
-        df2_1 = df2[selected_col_df2]
+        if selected_col_distribution and is_numeric_dtype(df_modify[selected_col_distribution]):
 
-    #=================================
+            distribution_expander.write('Minimum : ' + str(df_modify[selected_col_distribution].min()))
+            distribution_expander.write('Maximum : ' + str(df_modify[selected_col_distribution].max()))
+            distribution_expander.write('Mean : ' + str(df_modify[selected_col_distribution].mean()))
+            distribution_expander.write('Median : ' + str(df_modify[selected_col_distribution].median()))
+            distribution_expander.bar_chart(df_modify[selected_col_distribution])
+    
+        else:
+            distribution_expander.write('Invalid column (Non numeric)')
 
-    col_list_4 = list()
+#=================================================================================================================================================================
 
-    for c in df2_1:
-        col_list_4.append(c)
+        # create expander for [h]
+        unique_value = mcol3.beta_expander("Show unique values")
 
-    selected_col_df2_1_null = col1_sub3.multiselect('Select Columns to remove NULL [File 2]', col_list_4)
-    if selected_col_df2_1_null:
-        pass
-    else:
-        selected_col_df2_1_null = selected_col_df2
+        selected_col_unique = unique_value.selectbox('Select data field to show total unique value', col_list_modify)
+        
+        if selected_col_unique:
+            unique_set = set(df_modify[selected_col_unique])
+            unique_len = len(unique_set)
+            
+            unique_value.write('Number of items: ' + str(unique_len))
+            unique_value.write(unique_set)
 
-    if col1_sub3.checkbox('Remove NaN rows [File 2]'):
-        df2_1 = df2_1.dropna(subset=selected_col_df2_1_null)
+#=================================================================================================================================================================
+        
+        # create expander for [i]
+        download_file = mcol3.beta_expander("Download File")
+        if download_file.button('Export csv'):
+            x = download_link(df_modify, 'output.csv', 'Click here to download file')
+            download_file.markdown(x, unsafe_allow_html=True)
 
+        # display raw dataframe
+        tcol2.write("Dataframe")
+        tcol2.write(df_modify)
 
+        # crreate missingo chart to display data completeness
+        fig = msno.matrix(df_modify)
+        fig_copy = fig.get_figure()
+        fig_copy.savefig('raw.png', bbox_inches = 'tight')
 
-    #=================================
+        # display missingno chart
+        tcol3.write('Missingno chart')
+        tcol3.image('raw.png')
 
-	
-    selected_col_df2_1_replace = col1_sub4.multiselect('Select Columns to replace NULL values [File 2]', col_list_4)
+#=================================================================================================================================================================
 
-    if selected_col_df2_1_replace:
-        pass
-    else:
-        selected_col_df2_1_replace = selected_col_df2
-
-    replace_null_f2 = col1_sub4.radio('Replace NULL with [File 2]:', ('No action', '0','Mean', 'Median'))
-
-    if replace_null_f2 == 'No action':
-        pass
-    if replace_null_f2 == '0':
-        df2_1[selected_col_df2_1_replace] = df2_1[selected_col_df2_1_replace].fillna(0)
-    if replace_null_f2 == 'Mean':
-        df2_1[selected_col_df2_1_replace] = df2_1.fillna(df2_1[selected_col_df2_1_replace].mean())
-    if replace_null_f2 == 'Median':
-        df2_1[selected_col_df2_1_replace] = df2_1[selected_col_df2_1_replace].fillna(df2_1.median())
-
-
-    bcol2.write(df2_1)
-    bcol2.write(df2_1.shape)
-
-    if bcol2.button('Download [File 2] as CSV'):
-        tmp_download_link_file_2 = download_link(df2_1, 'file_2.csv', 'Click here to download your data!')
-        st.markdown(tmp_download_link_file_2, unsafe_allow_html=True)
-
+datacleanser()
 
 
 ###FILE MERGE####
@@ -229,3 +280,6 @@ try:
 
 except:
     pass
+
+
+
